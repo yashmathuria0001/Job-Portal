42b270bb65004b94d9cc4b8c5b1bd6d8b54c75fd
yash0001 <115033773+yashmathuria0001@users.noreply.github.com>
2024-12-25 02:09:28 +0530

    Jobs page has completed

--- Frontend/src/components/FilterCard.jsx
+++ Frontend/src/components/FilterCard.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
-
+import React from "react";
+import { RadioGroup, RadioGroupItem } from "./ui/radio-group";
+import { Label } from "./ui/label";
+const fitlerData = [
+  {
+    fitlerType: "Location",
+    array: ["Delhi NCR", "Bangalore", "Hyderabad", "Pune", "Mumbai"],
+  },
+  {
+    fitlerType: "Industry",
+    array: ["Frontend Developer", "Backend Developer", "FullStack Developer"],
+  },
+  {
+    fitlerType: "Salary",
+    array: ["0-40k", "42k-1lakh", "1lakh to 5lakh"],
+  },
+];
 const FilterCard = () => {
   return (
-    <h1></h1>
-  )
-}
+    <div className="w-full bg-white p-3 rounded-md">
+      <h1 className="font-bold text-lg">Filter Jobs</h1>
+      <hr className="font-bold text-lg" />
+      <RadioGroup>
+        {fitlerData.map((data, index) => (
+          <div>
+            <h1 className="font-bold text-lg">{data.fitlerType}</h1>
+            {data.array.map((item, index) => {
+              return (
+                <div className="flex items-center space-x-2 my-2">
+                  <RadioGroupItem value={item}  />
+                  <Label>{item}</Label>
+                </div>
+              );
+            })}
+          </div>
+        ))}
+      </RadioGroup>
+    </div>
+  );
+};
 
-export default FilterCard
+export default FilterCard;
