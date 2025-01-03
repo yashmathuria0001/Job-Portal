d7682715a9ba0233e770d078f82de54500dd7779
yash0001 <115033773+yashmathuria0001@users.noreply.github.com>
2025-01-03 18:28:03 +0530

    constant.js is updated for production server

--- Frontend/src/utils/constant.js
+++ Frontend/src/utils/constant.js
@@ -1,4 +1,6 @@
-export const USER_API_END_POINT="http://localhost:8000/api/v1/user";
-export const JOB_API_END_POINT="http://localhost:8000/api/v1/job";
-export const APPLICATION_API_END_POINT="http://localhost:8000/api/v1/application";
-export const COMPANY_API_END_POINT="http://localhost:8000/api/v1/company";
+const BASE_API_URL = "https://job-portal-z2n5-git-main-yash-mathurias-projects.vercel.app/api/v1" || 'http://localhost:8000/api/v1';
+
+export const USER_API_END_POINT = `${BASE_API_URL}/user`;
+export const JOB_API_END_POINT = `${BASE_API_URL}/job`;
+export const APPLICATION_API_END_POINT = `${BASE_API_URL}/application`;
+export const COMPANY_API_END_POINT = `${BASE_API_URL}/company`;
