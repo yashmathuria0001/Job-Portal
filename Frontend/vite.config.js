fc18be6af278d09eec9ada9f95ced55fa254342f
yash0001 <115033773+yashmathuria0001@users.noreply.github.com>
2025-01-05 01:41:14 +0530

    update vite config

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,12 +1,14 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from "path";
+import react from "@vitejs/plugin-react";
 
-export default defineConfig({
+export default {
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+  build: {
+    outDir: "dist", // Output directory
+  },
+};
