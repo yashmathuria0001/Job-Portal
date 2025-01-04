589e82b6d11722e6bbca0d10e32e5e5378b3d13f
yash0001 <115033773+yashmathuria0001@users.noreply.github.com>
2025-01-04 15:11:06 +0530

    update origin

--- Backend/index.js
+++ Backend/index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());//Parses cookies sent with incoming HTTP requests and makes them accessible through req.cookies.
 const corsOptions={
-    origin:['http://localhost:5173','https://job-portal-z2n5-git-main-yash-mathurias-projects.vercel.app/'],
+    origin:['http://localhost:5173','https://job-portal-z2n5.vercel.app/'],
     credentials:true
 }
 app.use(cors(corsOptions));
--- Frontend/src/utils/constant.js
+++ Frontend/src/utils/constant.js
@@ -1,4 +1,6 @@
-const BASE_API_URL = "https://job-portal-z2n5-git-main-yash-mathurias-projects.vercel.app/api/v1" || 'http://localhost:8000/api/v1';
+const BASE_API_URL = process.env.NODE_ENV === 'production' 
+  ? "https://job-portal-z2n5.vercel.app/api/v1" 
+  : 'http://localhost:8000/api/v1';
 
 export const USER_API_END_POINT = `${BASE_API_URL}/user`;
 export const JOB_API_END_POINT = `${BASE_API_URL}/job`;
