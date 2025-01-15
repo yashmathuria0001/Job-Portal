724d97f519773f6faa835f670ddc30aa1b5c7c3c
yash0001 <115033773+yashmathuria0001@users.noreply.github.com>
2025-01-16 00:11:44 +0530

    deploy on render

--- Backend/index.js
+++ Backend/index.js
@@ -7,10 +7,10 @@ import userRoute from "./routes/user.route.js";
 import companyRoute from "./routes/company.route.js";
 import jobRoute from "./routes/job.route.js";
 import applicationRoute from "./routes/application.route.js";
-
+import path from "path"
 dotenv.config({});
 
-
+const _diranme=path.resolve();
 const app=express();
 
 //middleware
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());//Parses cookies sent with incoming HTTP requests and makes them accessible through req.cookies.
 const corsOptions={
-    origin:['http://localhost:5173','https://job-portal-five-zeta.vercel.app/'],
+    origin:['http://localhost:5173','https://job-portal-z2n5.vercel.app/'],
     credentials:true
 }
 app.use(cors(corsOptions));
@@ -34,6 +34,11 @@ app.use("/api/v1/company",companyRoute);
 app.use("/api/v1/job", jobRoute);
 app.use("/api/v1/application", applicationRoute);
 
+app.use(express.static(path.join(_diranme,"/frontend/dist")))
+
+app.get('*',(req,res)=>{
+    res.sendFile(path.resolve(_dirname,"frontend","dist","index.html"));
+})
 app.listen(PORT,async()=>{
     await connectDB();
     console.log(`Server running at port ${PORT}`);
